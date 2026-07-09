c8de6825bb63151d66ae658e3462a2ca7f01a8c1
magloeagle-ux <magloeagle@gmail.com>
2026-07-10 00:45:43 +0300

    Update settings.py

--- threat_detection_system/settings.py
+++ threat_detection_system/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mock-key-for-development-purposes-only'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
